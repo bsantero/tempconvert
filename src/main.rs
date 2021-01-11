c797249562c55e10feff77c477a36a11af99eb0c
Brian Santero von der Embse <code@duckchase.com>
2021-01-11 12:18:28 +0100

    Add conversion comment.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
         // Parse the argument to turn it from string to a Float32
         let temp = arg1.parse::<f64>().unwrap();
 
+        // Convert the temperature
         let result = (temp - 32.0) * 5.0/9.0;
 
         // Print the damn temp!
